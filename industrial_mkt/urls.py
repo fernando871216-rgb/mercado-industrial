21bb262f1fd27b74d361be9ba3bd4f2ff7cbd181
fernando871216-rgb <fernando871216@gmail.com>
2026-01-07 05:06:55 -0600

    Update urls.py

--- industrial_mkt/urls.py
+++ industrial_mkt/urls.py
@@ -1,10 +1,22 @@
-from django.urls import path
 from . import views
+from django.contrib import admin
+from django.urls import path, include # Importante agregar 'include'
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
-    # Inicio y Detalles
     path('', views.home, name='home'),
     path('producto/<int:pk>/', views.product_detail, name='product_detail'),
+    path('subir/', views.subir_producto, name='subir_producto'),
+    path('editar/<int:pk>/', views.editar_producto, name='editar_producto'),
+    path('borrar/<int:pk>/', views.borrar_producto, name='borrar_producto'),
+    path('registro/', views.registro, name='registro'),
+    path('categoria/<int:category_id>/', views.category_detail, name='category_detail'),
+]
+
+# Esto es para que las imágenes de los productos se vean en desarrollo
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
     
     # Gestión de Productos
     path('subir/', views.subir_producto, name='subir_producto'),
@@ -15,3 +27,4 @@ urlpatterns = [
     path('registro/', views.registro, name='registro'),
     path('categoria/<int:category_id>/', views.category_detail, name='category_detail'),
 ]
+
