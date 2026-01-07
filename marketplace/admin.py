3605a076ffec6870f187a143434419b8d030b87b
fernando871216-rgb <fernando871216@gmail.com>
2026-01-06 23:42:43 -0600

    Update admin.py

--- marketplace/admin.py
+++ marketplace/admin.py
@@ -2,4 +2,10 @@ from django.contrib import admin
 from .models import Category, Product
 
 admin.site.register(Category)
+
 admin.site.register(Product)
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    # list_display = ('title', 'price', 'category') # Comenta esta línea con un #
+    pass
