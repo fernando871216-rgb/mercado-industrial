10ad026840a6835eca00ecc3bac9d33667d09e2f
fernando871216-rgb <fernando871216@gmail.com>
2026-01-06 19:16:06 -0600

    Add files via upload

--- industrial_mkt/urls.py
+++ industrial_mkt/urls.py
@@ -1,9 +1,9 @@
 from django.contrib import admin
 from django.urls import path
-from marketplace import views  # <--- ESTA LÍNEA ES LA QUE FALTA
+from marketplace import views 
+from django.contrib.auth import views as auth_views # Importante para login/logout
 from django.conf import settings
 from django.conf.urls.static import static
-from django.contrib.auth import views as auth_views # <--- AÑADE ESTO
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -11,11 +11,14 @@ urlpatterns = [
     path('vender/', views.subir_producto, name='vender'),
     path('registro/', views.registro, name='registro'),
     path('borrar/<int:product_id>/', views.borrar_producto, name='borrar_producto'),
+    
+    # Estas líneas arreglan el error de 'logout' y 'login'
     path('login/', auth_views.LoginView.as_view(template_name='marketplace/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(next_page='/'), name='logout'),
-
 ]
 
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 # Esto es para que se vean las fotos que subes
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
