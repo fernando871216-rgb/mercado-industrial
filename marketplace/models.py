a3b1088774bb59ecf983a7d3cd75507400423413
fernando871216-rgb <fernando871216@gmail.com>
2026-01-06 23:51:59 -0600

    Update models.py

--- marketplace/models.py
+++ marketplace/models.py
@@ -10,7 +10,7 @@ class Category(models.Model):
 class Product(models.Model):
     # Asegúrate de que esta línea esté idéntica:
     #category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='products', null=True, blank=True)
-    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, null=True, blank=True)
     seller = models.ForeignKey(User, on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
     brand = models.CharField(max_length=100)
@@ -24,4 +24,5 @@ class Product(models.Model):
     def __str__(self):
 
         return f"{self.brand} - {self.title}"
+
 
