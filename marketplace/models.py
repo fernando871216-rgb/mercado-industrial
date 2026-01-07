a2abc8ebec04a146aac00dff42e049eff918b1b0
fernando871216-rgb <fernando871216@gmail.com>
2026-01-07 17:17:41 -0600

    Update models.py

--- marketplace/models.py
+++ marketplace/models.py
@@ -1,5 +1,8 @@
 from django.db import models
 from django.conf import settings
+from cloudinary.models import CloudinaryField
+
+class IndustrialProduct(models.Model):
 
 # Modelo de Categorías
 class Category(models.Model):
@@ -37,7 +40,10 @@ class IndustrialProduct(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
+    image = CloudinaryField('image', null=True, blank=True)
+
     def __str__(self):
         return f"{self.title} - {self.brand}"
     
+
  
