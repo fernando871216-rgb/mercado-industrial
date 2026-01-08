f68720f2600667b0d1dfc8061dc6bcd06d0cd23d
fernando871216-rgb <fernando871216@gmail.com>
2026-01-07 23:50:18 -0600

    Update urls.py

--- marketplace/urls.py
+++ marketplace/urls.py
@@ -19,8 +19,10 @@ urlpatterns = [
     path('subir/', views.subir_producto, name='subir_producto'),
     path('editar/<int:pk>/', views.editar_producto, name='editar_producto'),
     path('borrar/<int:pk>/', views.borrar_producto, name='borrar_producto'),
+    path('mi-inventario/', views.mi_inventario, name='mi_inventario'),
     
     # Filtros
     path('categoria/<int:category_id>/', views.category_detail, name='category_detail'),
 ]
+
 
