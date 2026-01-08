d1dbb9f006e9290f55c9c32e14903593ac65efbd
fernando871216-rgb <fernando871216@gmail.com>
2026-01-07 19:30:40 -0600

    Update urls.py

--- marketplace/urls.py
+++ marketplace/urls.py
@@ -2,8 +2,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    # Página principal
+    path('admin/', admin.site.urls),
     path('', views.home, name='home'),
+    path('producto/<int:product_id>/', views.detalle_producto, name='product_detail'), # <--- AQUÍ FALTA LA COMA
+    path('pago-exitoso/', views.pago_exitoso, name='pago_exitoso'),
+]
     
     # Detalle de producto
     path('producto/<int:pk>/', views.product_detail, name='product_detail'),
@@ -13,11 +16,12 @@ urlpatterns = [
     path('subir/', views.subir_producto, name='subir_producto'),
     path('editar/<int:pk>/', views.editar_producto, name='editar_producto'),
     path('borrar/<int:pk>/', views.borrar_producto, name='borrar_producto'),
-    path('pago-exitoso/', views.pago_exitoso, name='pago_exitoso')
+    
     
     # Usuarios y Filtros
     path('registro/', views.registro, name='registro'),
     path('categoria/<int:category_id>/', views.category_detail, name='category_detail'),
 
 ]
+
 
