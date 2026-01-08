f9bdf19843ef0993ec7d2915da6af20a8e4a19b9
fernando871216-rgb <fernando871216@gmail.com>
2026-01-07 19:51:14 -0600

    Update urls.py

--- marketplace/urls.py
+++ marketplace/urls.py
@@ -1,20 +1,16 @@
-from django.contrib import admin
 from django.urls import path
-from marketplace import views 
+from . import views
 
 urlpatterns = [
-    # Administración
-    path('admin/', admin.site.urls),
-    
-    # Navegación Principal
+    # Navegación y Usuarios
     path('', views.home, name='home'),
     path('registro/', views.registro, name='registro'),
     
-    # Detalle y Pagos (Mercado Pago)
+    # Detalle de Producto y Mercado Pago
     path('producto/<int:product_id>/', views.detalle_producto, name='product_detail'),
     path('pago-exitoso/', views.pago_exitoso, name='pago_exitoso'),
     
-    # Gestión de productos
+    # Gestión de Productos
     path('subir/', views.subir_producto, name='subir_producto'),
     path('editar/<int:pk>/', views.editar_producto, name='editar_producto'),
     path('borrar/<int:pk>/', views.borrar_producto, name='borrar_producto'),
