18dc772876619bad7f228b02009ec2b0c3c675a8
fernando871216-rgb <fernando871216@gmail.com>
2026-01-08 18:54:40 -0600

    Update settings.py

--- industrial_mkt/settings.py
+++ industrial_mkt/settings.py
@@ -163,4 +163,12 @@ DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
 
+# Configuración para envío de correos
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'fernando871216@gmail.com'  # Tu cuenta de Gmail
+EMAIL_HOST_PASSWORD = 'Lasido871216.' # No es tu contraseña normal, es una especial de Google
+
 
