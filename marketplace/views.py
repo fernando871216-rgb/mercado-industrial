7b4c989162b62a55bd85ef92863d66bd82e39d5b
fernando871216-rgb <fernando871216@gmail.com>
2026-01-08 18:33:39 -0600

    Update views.py

--- marketplace/views.py
+++ marketplace/views.py
@@ -72,6 +72,12 @@ def mis_compras(request):
     compras = Sale.objects.filter(buyer=request.user).order_by('-created_at')
     return render(request, 'marketplace/mis_compras.html', {'compras': compras})
 
+@login_required
+def mis_ventas(request):
+    # Filtramos las ventas donde el vendedor del producto es el usuario actual
+    ventas = Sale.objects.filter(product__seller=request.user).order_by('-created_at')
+    return render(request, 'marketplace/mis_ventas.html', {'ventas': ventas})
+
 # 3. PÁGINA DE ÉXITO (Baja de Stock)
 def pago_exitoso(request):
     producto_id = request.GET.get('external_reference')
@@ -158,6 +164,7 @@ def category_detail(request, category_id):
     category = get_object_or_404(Category, id=category_id)
     products = IndustrialProduct.objects.filter(category=category)
     return render(request, 'marketplace/inicio.html', {'products': products, 'category': category})
+
 
 
 
