2f98beb681dd188c3720642643281519f1af6d68
fernando871216-rgb <fernando871216@gmail.com>
2026-01-08 19:28:15 -0600

    Update urls.py

--- marketplace/urls.py
+++ marketplace/urls.py
@@ -10,6 +10,7 @@ urlpatterns = [
     # LOGIN Y LOGOUT (Agregamos estas dos)
     path('login/', auth_views.LoginView.as_view(template_name='marketplace/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(next_page='home'), name='logout'),
+    path('perfil/editar/', views.editar_perfil, name='editar_perfil'),
     
     # Detalle de Producto y Mercado Pago
     path('producto/<int:product_id>/', views.detalle_producto, name='product_detail'),
@@ -27,6 +28,7 @@ urlpatterns = [
     # Filtros
     path('categoria/<int:category_id>/', views.category_detail, name='category_detail'),
 ]
+
 
 
 
