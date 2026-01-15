8160fc22dd2fe7676ae80f4c30e57c4924a39429
fernando871216-rgb <fernando871216@gmail.com>
2026-01-15 05:54:58 -0600

    Update settings.py

--- industrial_mkt/settings.py
+++ industrial_mkt/settings.py
@@ -93,8 +93,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'fernando871216@gmail.com'
-EMAIL_HOST_PASSWORD = 'pgwj tvdh mwjz pyrc'
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
 DEFAULT_FROM_EMAIL = 'Mercado Industrial <fernando871216@gmail.com>'
 
 # INTERNACIONALIZACIÓN
@@ -103,3 +103,4 @@ TIME_ZONE = 'America/Mexico_City' # Horario de CDMX
 USE_I18N = True
 USE_TZ = True
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
