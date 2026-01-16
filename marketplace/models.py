3493cc6d2cb714b66f20e6d117e0869b9b82edcd
fernando871216-rgb <fernando871216@gmail.com>
2026-01-15 18:48:06 -0600

    Update models.py

--- marketplace/models.py
+++ marketplace/models.py
@@ -58,7 +58,6 @@ class Sale(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     status = models.CharField(max_length=20, default='pendiente')
     pagado_a_vendedor = models.BooleanField(default=False)
-    payment_id = models.CharField(max_length=100, null=True, blank=True)
     recibido_por_comprador = models.BooleanField(default=False)
     tracking_number = models.CharField(max_length=100, blank=True, null=True, verbose_name="Número de Guía")
     shipping_company = models.CharField(max_length=50, blank=True, null=True, verbose_name="Paquetería")
@@ -96,6 +95,7 @@ def save_user_profile(sender, instance, **kwargs):
         instance.profile.save()
     except Profile.DoesNotExist:
         Profile.objects.create(user=instance)
+
 
 
 
