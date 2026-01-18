b40c876b6b72081a382f7c59eca2996ad0bb91e8
fernando871216-rgb <fernando871216@gmail.com>
2026-01-18 17:34:27 -0600

    Update urls.py

--- industrial_mkt/urls.py
+++ industrial_mkt/urls.py
@@ -4,6 +4,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib.auth import views as auth_views # Importa las vistas de autenticación
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('marketplace.urls')),
@@ -11,6 +12,10 @@ urlpatterns = [
     path('logout/', auth_views.LogoutView.as_view(next_page='home'), name='logout'),
     path('login/', auth_views.LoginView.as_view(template_name='marketplace/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(next_page='home'), name='logout'),
+    path('reset_password/', auth_views.PasswordResetView.as_view(template_name="registration/password_reset.html"), name="password_reset"),
+    path('reset_password_sent/', auth_views.PasswordResetDoneView.as_view(template_name="registration/password_reset_sent.html"), name="password_reset_done"),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name="registration/password_reset_form.html"), name="password_reset_confirm"),
+    path('reset_password_complete/', auth_views.PasswordResetCompleteView.as_view(template_name="registration/password_reset_done.html"), name="password_reset_complete"),
 ]
 
 
@@ -20,6 +25,7 @@ if settings.DEBUG:
 else:
     # En Render (producción), esto ayuda a que las imágenes se sirvan si usas WhiteNoise
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
 
 
 
