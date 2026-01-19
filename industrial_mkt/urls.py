8d6c16716a1fa2dcc5522c3f2cb05b122ba0bd95
fernando871216-rgb <fernando871216@gmail.com>
2026-01-18 22:22:32 -0600

    Update urls.py

--- industrial_mkt/urls.py
+++ industrial_mkt/urls.py
@@ -2,40 +2,33 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from django.contrib.auth import views as auth_views # Importa las vistas de autenticación
-
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('marketplace.urls')),
-    # Agrega esta línea para el logout:
-    path('logout/', auth_views.LogoutView.as_view(next_page='home'), name='logout'),
+    
+    # Autenticación
     path('login/', auth_views.LoginView.as_view(template_name='marketplace/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(next_page='home'), name='logout'),
+    
+    # Recuperación de Contraseña
     path('reset_password/', auth_views.PasswordResetView.as_view(template_name="registration/password_reset.html"), name="password_reset"),
+    
     path('reset_password_sent/', auth_views.PasswordResetDoneView.as_view(template_name="registration/password_reset_sent.html"), name="password_reset_done"),
-    path('reset_password_complete/', auth_views.PasswordResetCompleteView.as_view(template_name="registration/password_reset_done.html"), name="password_reset_complete"),
-   path('reset/<uidb64>/<token>/', 
+    
+    path('reset/<uidb64>/<token>/', 
          auth_views.PasswordResetConfirmView.as_view(
              template_name="registration/password_reset_confirm.html",
              success_url='/reset_password_complete/'
          ), 
          name="password_reset_confirm"),
+    
+    path('reset_password_complete/', auth_views.PasswordResetCompleteView.as_view(template_name="registration/password_reset_done.html"), name="password_reset_complete"),
 ]
 
-
-# Configuración para servir archivos multimedia (imágenes de productos)
+# Configuración para servir archivos multimedia
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 else:
-    # En Render (producción), esto ayuda a que las imágenes se sirvan si usas WhiteNoise
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
-
-
-
-
-
-
-
-
