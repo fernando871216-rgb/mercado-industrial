3c0ea180294b70e965d9effb91db6f5132e85edd
fernando871216-rgb <fernando871216@gmail.com>
2026-01-18 22:19:54 -0600

    Update urls.py

--- industrial_mkt/urls.py
+++ industrial_mkt/urls.py
@@ -14,16 +14,13 @@ urlpatterns = [
     path('logout/', auth_views.LogoutView.as_view(next_page='home'), name='logout'),
     path('reset_password/', auth_views.PasswordResetView.as_view(template_name="registration/password_reset.html"), name="password_reset"),
     path('reset_password_sent/', auth_views.PasswordResetDoneView.as_view(template_name="registration/password_reset_sent.html"), name="password_reset_done"),
-    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name="registration/password_reset_form.html"), name="password_reset_confirm"),
     path('reset_password_complete/', auth_views.PasswordResetCompleteView.as_view(template_name="registration/password_reset_done.html"), name="password_reset_complete"),
-    path('reset/<uidb64>/<token>/', 
-     auth_views.PasswordResetConfirmView.as_view(
-         template_name='registration/password_reset_confirm.html', # O admin/registration/password_reset_confirm.html si moviste la carpeta
-         success_url='/reset_password_complete/'
-     ), 
-     name='password_reset_confirm'),
-     ), 
-     name="password_reset_confirm"),
+   path('reset/<uidb64>/<token>/', 
+         auth_views.PasswordResetConfirmView.as_view(
+             template_name="registration/password_reset_confirm.html",
+             success_url='/reset_password_complete/'
+         ), 
+         name="password_reset_confirm"),
 ]
 
 
@@ -33,6 +30,7 @@ if settings.DEBUG:
 else:
     # En Render (producción), esto ayuda a que las imágenes se sirvan si usas WhiteNoise
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
 
 
 
