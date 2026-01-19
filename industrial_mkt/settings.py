786e687d24e7a854180654e2634eca1741579780
fernando871216-rgb <fernando871216@gmail.com>
2026-01-18 22:12:07 -0600

    Update settings.py

--- industrial_mkt/settings.py
+++ industrial_mkt/settings.py
@@ -105,6 +105,7 @@ EMAIL_USE_TLS = True
 EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASSWORD')
 DEFAULT_FROM_EMAIL = 'Mercado Industrial INITRE <iniestaedgar875@gmail.com>'
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
 # INTERNACIONALIZACIÓN
 LANGUAGE_CODE = 'es-mx'
@@ -112,6 +113,7 @@ TIME_ZONE = 'America/Mexico_City'
 USE_I18N = True
 USE_TZ = True
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
 
 
 
